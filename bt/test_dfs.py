5c19c1464cd9f86a9e822429832244b894193e57
Donald Ballard <github@itsdj.ca>
2024-10-18 14:37:30 -0700

    swap values in assert statements

--- bt/test_dfs.py
+++ bt/test_dfs.py
@@ -9,25 +9,25 @@ class TestDFS(unittest.TestCase):
         found = DFS().search(root, 7)
         self.assertEqual(found.val, 7)
         found4 = DFS().search(root, 4)
-        self.assertEqual(found4.val, 4)
+        self.assertEqual(4, found4.val)
 
     def test_dfs_single_tree(self):
         test_tree = iter("3".split())
         root = build_tree(test_tree, int)
         found3 = DFS().search(root, 3)
-        self.assertEqual(found3.val, 3)
+        self.assertEqual(3, found3.val)
 
     def test_dfs_global_search(self):
         test_tree = iter("3 4 5 x x 7 x x 8 x x".split())
         root = build_tree(test_tree, int)
         found = DFS().search_global(root, 5)
-        self.assertEqual(found.val, 5)
+        self.assertEqual(5, found.val)
 
     def test_dfs_find_highest(self):
         test_tree = iter("3 4 5 x x 7 x x 8 x x".split())
         root = build_tree(test_tree, int)
         highest = DFS().find_highest_val(root)
-        self.assertEqual(highest, 8)
+        self.assertEqual(8, highest)
 
     def test_dfs_find_max_depth(self):
         test_tree = iter("3 4 5 x x 7 x x 8 x x".split())
